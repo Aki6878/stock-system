60e1351e23651426c48c714763dc6d50063f69ba
Aki6878 <roadream.akihito.ishiyama@gmail.com>
2026-06-09 15:34:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.post('/api/stock', (req, res) => {
     if (!item) {
         item = { shelfId, productName, productId, maker, quantity: 0 };
         stockDatabase.push(item);
+    } else {
+        item.productName = productName;
+        item.maker = maker;
     }
     
     if (type === 'in') item.quantity += quantity;
